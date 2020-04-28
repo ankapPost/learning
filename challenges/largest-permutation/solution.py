5db199c7a5313992d69c38d1b9d1c87bbb9a0f1a
ankapPost <60682307+ankapPost@users.noreply.github.com>
2020-04-28 11:17:47 +0400

    Update solution.py

--- challenges/largest-permutation/solution.py
+++ challenges/largest-permutation/solution.py
@@ -7,22 +7,26 @@ import re
 import sys
 
 def largestPermutation(k, arr):
+    n = len(arr)
+    pos = {}
+    for i in range(n):
+        pos[arr[i]] = i
+    
     ki = 0
-    ci = 0
-    while (ki < k) and (ci < len(arr)):
-        max = arr[ci]
-        max_index = ci
-        max_found = 0
-        for i in range(ci+1,len(arr)):
-            if arr[i] > max:
-                max = arr[i]
-                max_index = i
-                max_found = 1
-        if max_found == 1:
-            arr[max_index] = arr[ci]
-            arr[ci] = max
-            ki = ki + 1
-        ci = ci +1
+    i = 0
+    while (ki < k) and (i < n):
+        if arr[i] == n-i:
+            i = i +1
+            continue
+        tmp = pos[n-i]
+        
+        pos[arr[i]] = pos[n-i]
+        pos[n-i] = i 
+        
+        arr[i], arr[tmp] = arr[tmp], arr[i]
+        
+        i = i +1
+        ki = ki + 1
     return arr
 
 
