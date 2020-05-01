78ec9184fea615b986f3030b33d0b501fdc457cf
ankapPost <60682307+ankapPost@users.noreply.github.com>
2020-05-01 19:15:16 +0400

    Update soulution.py

--- challenges/picking-numbers/soulution.py
+++ challenges/picking-numbers/soulution.py
@@ -13,14 +13,21 @@ import sys
 # The function accepts INTEGER_ARRAY a as parameter.
 #
 def pickingNumbers(a):
-    a.sort()
+#    a.sort()
     max = 0
     for i in range(len(a)):
         count = 0
+        countplus = 0
+        countminus =0
         for j in range(i,len(a)):
-            if a[j]-a[i]<=1:
-                count = count + 1
-            print(a[i],max,count)
+            if a[j]>=a[i] and abs(a[j]-a[i])<=1:
+                countplus = countplus + 1
+            if a[j]<=a[i] and abs(a[j]-a[i])<=1:
+                countminus = countminus + 1
+            if countplus > countminus:
+                count = countplus
+            else:
+                count = countminus
         if count > max:
             max = count
     return max
